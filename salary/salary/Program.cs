eeb9027d3f94e3236c01d6093407681ee6479a71
uxavera96@gmail.com <uxavera96@gmail.com>
2019-11-24 14:26:38 -0800

    another to added

---
+++ salary/salary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Employee1=new SalaryCalculator( 1, "danny", "section");
+            Console.WriteLine("your net salary: " + SalaryCalculator.CalculateSalary(100000));
+            Console.WriteLine("your Hourly salary:"+SalaryCalculator.CalculateHourly(12, 34));
+            Console.WriteLine("your Hourly salary:"+ SalaryCalculator.CalculateDaily(12));
+            
+        }
+    }
+}
---
+++ salary/salary/salarycalcutor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class SalaryCalculator
+    {
+        public int Empid;
+        public string Name;
+        public string Position;
+
+        public SalaryCalculator(int empid, string name, string  position)
+        {
+            this.Empid = empid;
+            this.Name = name;
+            this.Position = position;
+        }
+
+        public void test()
+        {
+            Console.WriteLine("SalaryCalculator");
+        }
+        
+        public static double CalculateSalary(double Gs)
+        {
+            double Salary;
+            Salary=Gs-(Gs*0.3) - (Gs*0.05) - (Gs*0.03);
+            return Salary;
+        }
+        public static double CalculateHourly(double RT, double NumberOfOur )
+        {
+            double SalaryHour;
+            SalaryHour=(RT*NumberOfOur)-0.05-0.03;
+            return SalaryHour;
+        }
+        public static double CalculateDaily(double Gs)
+        {
+            double SalaryDay;
+            SalaryDay=Gs-(Gs*0.3) ;
+            return SalaryDay;
+        }
+    }
+}
